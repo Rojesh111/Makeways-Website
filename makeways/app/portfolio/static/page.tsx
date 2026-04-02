3fdf1cf9a511d5edc2ad1596388bd898556d80b6
Sandip Rai <su220360.bsit@presidential.edu.np>
2026-04-02 21:44:00 +0545

    portfolio categories and images were added.

--- makeways/app/portfolio/static/page.tsx
+++ makeways/app/portfolio/static/page.tsx
@@ -3,21 +3,71 @@
 import PortfolioCategory from '@/components/PortfolioCategory';
 
 const items = [
-  { id: 1,  title: 'Social Media Campaign',  client: 'CLIENT NAME', year: '2024', isVideo: false, src: '' },
-  { id: 2,  title: 'Influencer Series',       client: 'CLIENT NAME', year: '2024', isVideo: true,  src: '' },
-  { id: 3,  title: 'Motion Graphics',         client: 'CLIENT NAME', year: '2024', isVideo: true,  src: '' },
-  { id: 4,  title: 'Digital Campaign',        client: 'CLIENT NAME', year: '2023', isVideo: false, src: '' },
-  { id: 5,  title: 'Viral Post Series',       client: 'CLIENT NAME', year: '2024', isVideo: false, src: '' },
-  { id: 6,  title: 'App Launch Campaign',     client: 'CLIENT NAME', year: '2023', isVideo: true,  src: '' },
+  {
+    id     : 1,
+    title  : 'Social Media Campaign',
+    client : 'CLIENT NAME',
+    year   : '2024',
+    images : [
+      '/portfolio/static/project1/img1.jpg',
+      '/portfolio/static/project1/img2.jpg',
+      '/portfolio/static/project1/img3.jpg',
+    ],
+  },
+  {
+    id     : 2,
+    title  : 'Brand Identity',
+    client : 'CLIENT NAME',
+    year   : '2024',
+    images : '/portfolio/static/project2/cover.jpg',
+  },
+  {
+    id     : 3,
+    title  : 'Poster Series',
+    client : 'CLIENT NAME',
+    year   : '2024',
+    images : [
+      '/portfolio/static/project3/img1.jpg',
+      '/portfolio/static/project3/img2.jpg',
+      '/portfolio/static/project3/img3.jpg',
+      '/portfolio/static/project3/img4.jpg',
+      '/portfolio/static/project3/img5.jpg',
+    ],
+  },
+  {
+    id     : 4,
+    title  : 'Digital Campaign',
+    client : 'CLIENT NAME',
+    year   : '2023',
+    images : '/portfolio/static/project4/cover.jpg',
+  },
+  {
+    id     : 5,
+    title  : 'Viral Post Series',
+    client : 'CLIENT NAME',
+    year   : '2024',
+    images : [
+      '/portfolio/static/project5/img1.jpg',
+      '/portfolio/static/project5/img2.jpg',
+    ],
+  },
+  {
+    id     : 6,
+    title  : 'App Launch Campaign',
+    client : 'CLIENT NAME',
+    year   : '2023',
+    images : '/portfolio/static/project6/cover.jpg',
+  },
+  // Add more projects here...
 ];
 
 export default function StaticPage() {
   return (
     <PortfolioCategory
-      title="STATIC"
-      subtitle="Print & Digital"
-      accent="#FF8C00"
-      items={items}
+      title    = "STATIC"
+      subtitle = "Print & Digital"
+      accent   = "#f47c20"
+      items    = {items}
     />
   );
 }
